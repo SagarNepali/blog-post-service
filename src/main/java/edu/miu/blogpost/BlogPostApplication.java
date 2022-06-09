b8fa6e057ed1cee14d4a06eb63e93e0f9c4dd04a
Sagar <worthless.dumboo@gmail.com>
2022-06-09 18:39:57 -0500

    Initial commit

---
+++ src/main/java/edu/miu/blogpost/BlogPostApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.blogpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPostApplication.class, args);
+	}
+
+}
