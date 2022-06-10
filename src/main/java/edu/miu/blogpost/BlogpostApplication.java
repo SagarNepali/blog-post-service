f9e8693fec7aa8561eb7f37420e1918113bcc9cc
Sagar <worthless.dumboo@gmail.com>
2022-06-10 11:14:05 -0500

    Project structure added

---
+++ src/main/java/edu/miu/blogpost/BlogpostApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.blogpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpostApplication.class, args);
+	}
+
+}
